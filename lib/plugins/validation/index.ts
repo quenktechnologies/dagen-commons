83ef484895459d027dc2d85f456b8f89468a5455
Lasana Murray <lmurray@quenk.com>
2019-07-28 18:27:54 -0400

    refactor(plugins/validation): add hasPrec

--- lib/plugins/validation/index.ts
+++ lib/plugins/validation/index.ts
@@ -51,6 +51,8 @@ export class ValidationPlugin extends AbstractPlugin {
 
         g.env.addGlobal('imports2TS', toString);
 
+        g.env.addGlobal('hasPrec', (s: Schema) => (s[this.key] != null));
+
         g.env.addGlobal('getPrec', (s: Schema) => s[this.key]);
 
         return pure(g);
